01ebb0dff529ae429932883942e11c3c68be9ca0
lsh <viperact26@gmail.com>
2023-01-26 21:51:56 +0900

    update

---
+++ springboot_03_board/src/main/java/com/example/board/Springboot03BoardApplication.java
@@ -0,0 +1,13 @@
+package com.example.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03BoardApplication.class, args);
+	}
+
+}
